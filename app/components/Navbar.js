e49474ac3d3fa0258cdef27572646837a6c611dd
xaf13 <xavierayden13@gmail.com>
2026-09-11 20:43:51 -0400

    Refactor Navbar links and remove login button
    Updated links in Navbar component to use lowercase paths and removed the login button.

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,3 +1,4 @@
+```jsx
 export default function Navbar() {
   return (
     <nav className="navbar">
@@ -7,18 +8,15 @@ export default function Navbar() {
 
       <div className="nav-links">
         <a href="/">Home</a>
-        <a href="/Rules">Rules</a>
+        <a href="/rules">Rules</a>
         <a href="/games">Games</a>
-        <a href="/online chat">Online Chat</a>
+        <a href="/chat">Online Chat</a>
         <a href="/clans">Clans</a>
         <a href="/profile">Profile</a>
         <a href="/report">Report</a>
         <a href="/settings">Settings</a>
       </div>
-
-      <a className="login-button" href="/login">
-        Log In
-      </a>
     </nav>
   );
 }
+```
