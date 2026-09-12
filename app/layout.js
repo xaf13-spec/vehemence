282df36807815b0a3779e0e87d5a86369ca6f400
xaf13 <xavierayden13@gmail.com>
2026-09-11 23:13:11 -0400

    Add floating chat widget to layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import "./themes.css";
 import "./fonts.css";
 import Navbar from "./components/Navbar";
+import ChatWidget from "./components/ChatWidget";
 
 export const metadata = {
   title: "Vehemence",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         {children}
+        <ChatWidget />
       </body>
     </html>
   );
