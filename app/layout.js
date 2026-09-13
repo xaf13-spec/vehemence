f45bc39572987d68453c693cf0553666d2948a72
xaf13 <xavierayden13@gmail.com>
2026-09-13 02:28:20 -0400

    Pass current user into single-page app shell

--- app/layout.js
+++ app/layout.js
@@ -9,23 +9,25 @@ import "./page-transition.css";
 import "./navigation.css";
 import "./friends/friends.css";
 import "./soundboard/soundboard.css";
-import "./persistent-spotify.css";
 import ChatWidget from "./components/ChatWidget";
 import CallWidget from "./components/CallWidget";
 import BrowserCustomization from "./components/BrowserCustomization";
 import AppShell from "./components/AppShell";
+import { getCurrentUser } from "../lib/auth";
 
 export const metadata = {
   title: "Vehemence",
   description: "Vehemence - community, music, and more."
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const user = await getCurrentUser();
+
   return (
     <html lang="en">
       <body>
         <BrowserCustomization />
-        <AppShell>{children}</AppShell>
+        <AppShell user={user}>{children}</AppShell>
         <ChatWidget />
         <CallWidget />
       </body>
