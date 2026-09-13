ae30bcc2158b1bf484011f00943d4b464d0223a3
xaf13 <xavierayden13@gmail.com>
2026-09-13 02:14:54 -0400

    Remove Games page

--- app/games/page.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { getCurrentUser } from "../../lib/auth";
-import GamesClient from "./GamesClient";
-
-export default async function Games() {
-  const user = await getCurrentUser();
-  if (!user) redirect("/rules");
-  return <GamesClient />;
-}
