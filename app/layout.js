646c15d4c4ebb5678e1a75e32efb9a1d2caee5d5
xaf13 <xavierayden13@gmail.com>
2026-09-13 02:14:03 -0400

    Remove games and persistent music imports

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,10 @@ import "./profile.css";
 import "./settings.css";
 import "./chat-widget.css";
 import "./call-widget.css";
-import "./game-player.css";
 import "./page-transition.css";
 import "./navigation.css";
 import "./friends/friends.css";
 import "./soundboard/soundboard.css";
-import "./persistent-spotify.css";
 import ChatWidget from "./components/ChatWidget";
 import CallWidget from "./components/CallWidget";
 import BrowserCustomization from "./components/BrowserCustomization";
@@ -18,7 +16,7 @@ import AppShell from "./components/AppShell";
 
 export const metadata = {
   title: "Vehemence",
-  description: "Vehemence - Games, community, and more."
+  description: "Vehemence - community, music, and more."
 };
 
 export default function RootLayout({ children }) {
