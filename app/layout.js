9d6d4941fc4c635a6f862dc4b4ae633ea65de86d
xaf13 <xavierayden13@gmail.com>
2026-09-13 05:57:53 -0400

    Load entertainment player styles

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ import "./friends/friends.css";
 import "./soundboard/soundboard.css";
 import "./music/music.css";
 import "./social-dashboard.css";
+import "./entertainment/entertainment.css";
 import ChatWidget from "./components/ChatWidget";
 import CallWidget from "./components/CallWidget";
 import BrowserCustomization from "./components/BrowserCustomization";
